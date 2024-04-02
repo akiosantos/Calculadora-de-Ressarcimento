9784cce6377c5372b24904e586ca51e0ec620011
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-04-02 00:08:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,22 @@ function formatarFaturamentoTotal() {
     var faturamentoTotalInput = document.getElementById("faturamento-total");
     var valor = faturamentoTotalInput.value;
 
-    // Remover todos os caracteres não numéricos, exceto ponto
+    // Remover caracteres não numéricos, exceto ponto e números
     valor = valor.replace(/[^\d.]/g, '');
 
-    // Verificar se o valor já contém um ponto decimal
-    var temPontoDecimal = valor.indexOf('.') !== -1;
-
-    // Se não houver ponto decimal, adicionar automaticamente no final do número
-    if (!temPontoDecimal && valor !== '') {
-        valor += '.';
-    }
+    // Adicionar ponto como separador de milhares
+    valor = valor.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
     // Atualizar o valor no campo
     faturamentoTotalInput.value = valor;
 }
-
 
 function calcularRessarcimento() {
     // Chama a função formatarFaturamentoTotal() para formatar o valor corretamente
     formatarFaturamentoTotal();
 
     var faturamentoTotalInput = document.getElementById("faturamento-total");
-    var faturamentoTotalValue = faturamentoTotalInput.value.trim().replace('R$', '').trim();
+    var faturamentoTotalValue = faturamentoTotalInput.value.trim().replace('R$', '').replace(/\./g, '').trim();
     
     // Converter para número
     var faturamentoTotal = parseFloat(faturamentoTotalValue);
@@ -37,9 +31,13 @@ function calcularRessarcimento() {
 
     var baseCalculo = faturamentoTotal * 0.012;
     var saldoMedio = (baseCalculo * meses) - 0.20;
-
+    
     // Formatar o resultado sem arredondamento
     var saldoMedioFormatado = saldoMedio.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 
     document.getElementById("resultado").innerText = "O saldo médio é: R$" + saldoMedioFormatado;
 }
+
+// Adicionar um ouvinte de evento de entrada para o campo "Faturamento Total"
+var faturamentoTotalInput = document.getElementById("faturamento-total");
+faturamentoTotalInput.addEventListener("input", formatarFaturamentoTotal);
