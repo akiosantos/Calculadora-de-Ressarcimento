d977714592b10797790b59f3f6c23223fbdb970f
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-04-02 15:38:39 -0300

    Update script.js
    Chamar a função para calcular o ressarcimento

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Função para formatar o valor do faturamento total
 function formatarFaturamentoTotal() {
   var faturamentoTotalInput = document.getElementById("faturamento-total");
   var valor = faturamentoTotalInput.value.trim().replace('R$', ''); // Remover o símbolo de "R$"
@@ -10,36 +11,34 @@ function formatarFaturamentoTotal() {
 
   // Atualizar o valor no campo
   faturamentoTotalInput.value = "R$ " + valor;
-}
 
   // Chamar a função para calcular o ressarcimento
   calcularRessarcimento();
+}
 
 // Chama a função formatarFaturamentoTotal() sempre que o usuário digitar algo
 document.getElementById("faturamento-total").addEventListener("input", formatarFaturamentoTotal);
 
+// Função para calcular o ressarcimento
 function calcularRessarcimento() {
-  // Chama a função formatarFaturamentoTotal() para formatar o valor corretamente
-  formatarFaturamentoTotal();
-
   var faturamentoTotalInput = document.getElementById("faturamento-total");
   var faturamentoTotalValue = faturamentoTotalInput.value.trim().replace('R$', '').replace(/\./g, '').trim();
   
-    // Converter para número
-    var faturamentoTotal = parseFloat(faturamentoTotalValue);
+  // Converter para número
+  var faturamentoTotal = parseFloat(faturamentoTotalValue);
 
-    if (isNaN(faturamentoTotal)) {
-        alert("Por favor, insira um valor válido para o Faturamento Total.");
-        return;
-    }
+  if (isNaN(faturamentoTotal)) {
+      alert("Por favor, insira um valor válido para o Faturamento Total.");
+      return;
+  }
 
-    var meses = parseInt(document.getElementById("meses").value);
+  var meses = parseInt(document.getElementById("meses").value);
 
-    var baseCalculo = faturamentoTotal * 0.012;
-    var saldoMedio = (baseCalculo * meses) - 0.20; // Subtrai 20%
+  var baseCalculo = faturamentoTotal * 0.012;
+  var saldoMedio = (baseCalculo * meses) - 0.20; // Subtrai 20%
 
-    // Formatar o resultado sem arredondamento
-    var saldoMedioFormatado = saldoMedio.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+  // Formatar o resultado sem arredondamento
+  var saldoMedioFormatado = saldoMedio.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 
-    document.getElementById("resultado").innerText = "O saldo médio é: R$ " + saldoMedioFormatado;
+  document.getElementById("resultado").innerText = "O saldo médio é: R$ " + saldoMedioFormatado;
 }
