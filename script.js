848c25ea8c3b616fed261a27ca883267febb8923
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-04-01 23:46:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
+function formatarNumero(valor) {
+    // Remover todos os caracteres não numéricos, exceto ponto
+    valor = valor.replace(/[^\d.]/g, '');
+
+    // Remover todos os pontos, exceto o último
+    valor = valor.replace(/\.(?=.*\.)/g, '');
+
+    // Substituir a vírgula por ponto
+    valor = valor.replace(',', '.');
+
+    return valor;
+}
+
+function formatarFaturamentoTotal() {
+    var faturamentoTotalInput = document.getElementById("faturamento-total");
+    var valor = faturamentoTotalInput.value;
+
+    // Formatar o valor enquanto o usuário digita
+    valor = formatarNumero(valor);
+
+    // Atualizar o valor no campo
+    faturamentoTotalInput.value = valor;
+}
+
 function calcularRessarcimento() {
     var faturamentoTotalInput = document.getElementById("faturamento-total");
     var faturamentoTotalValue = faturamentoTotalInput.value.trim().replace('R$', '').trim();
     
-    // Remover quaisquer caracteres não numéricos (exceto vírgula)
-    faturamentoTotalValue = faturamentoTotalValue.replace(/[^\d,]/g, '');
-
-    // Converter a vírgula para ponto para garantir que possamos parsear corretamente para um número
-    faturamentoTotalValue = faturamentoTotalValue.replace(',', '.');
+    // Remover quaisquer caracteres não numéricos (exceto ponto)
+    faturamentoTotalValue = formatarNumero(faturamentoTotalValue);
 
     // Converter para número
     var faturamentoTotal = parseFloat(faturamentoTotalValue);
@@ -19,7 +40,7 @@ function calcularRessarcimento() {
     var meses = parseInt(document.getElementById("meses").value);
 
     var baseCalculo = faturamentoTotal * 0.012;
-    var saldoMedio = baseCalculo * meses; // Agora não subtraímos os honorários
+    var saldoMedio = baseCalculo * meses;
 
     // Formatando o resultado em reais (R$)
     var saldoMedioFormatado = saldoMedio.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
