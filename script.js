312c0d558bd69292b5efa06954d2783f2e673cfe
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-04-02 20:59:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ function formatarFaturamentoTotal() {
   }
 
 //com R$
-var f = atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+var faturamentoTotalInput = atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 
 //sem R$
-var f2 = atual.toLocaleString('pt-br', {minimumFractionDigits: 2});
+var faturamentoTotalInput = atual.toLocaleString('pt-br', {minimumFractionDigits: 2});
 
 console.log(f);
 console.log(f2);
