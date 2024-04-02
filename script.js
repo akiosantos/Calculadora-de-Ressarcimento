5f1dee181352ef349db4f460450c82a10228e3d3
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-04-01 23:32:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function calcularRessarcimento() {
-    var faturamentoTotal = parseFloat(document.getElementById("faturamento-total").value);
+    var faturamentoTotalInput = document.getElementById("faturamento-total");
+    var faturamentoTotalValue = faturamentoTotalInput.value.trim().replace('R$', '').trim();
+    
+    // Remover quaisquer caracteres não numéricos (exceto vírgula)
+    faturamentoTotalValue = faturamentoTotalValue.replace(/[^\d,]/g, '');
+
+    // Converter a vírgula para ponto para garantir que possamos parsear corretamente para um número
+    faturamentoTotalValue = faturamentoTotalValue.replace(',', '.');
+
+    // Converter para número
+    var faturamentoTotal = parseFloat(faturamentoTotalValue);
+
+    if (isNaN(faturamentoTotal)) {
+        alert("Por favor, insira um valor válido para o Faturamento Total.");
+        return;
+    }
+
     var meses = parseInt(document.getElementById("meses").value);
 
     var baseCalculo = faturamentoTotal * 0.012;
