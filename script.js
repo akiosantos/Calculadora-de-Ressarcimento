44dd59163af8d914305542ac65d2a7a8f3428ce2
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-04-02 00:12:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ function formatarFaturamentoTotal() {
     var faturamentoTotalInput = document.getElementById("faturamento-total");
     var valor = faturamentoTotalInput.value;
 
-    // Remover caracteres não numéricos, exceto ponto e números
+    // Remover todos os caracteres não numéricos, exceto ponto
     valor = valor.replace(/[^\d.]/g, '');
 
     // Adicionar ponto como separador de milhares
-    valor = valor.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    var partes = valor.split('.');
+    var parteInteira = partes[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    var parteDecimal = partes.length > 1 ? '.' + partes[1] : '';
 
     // Atualizar o valor no campo
-    faturamentoTotalInput.value = valor;
+    faturamentoTotalInput.value = parteInteira + parteDecimal;
 }
 
 function calcularRessarcimento() {
@@ -30,8 +32,8 @@ function calcularRessarcimento() {
     var meses = parseInt(document.getElementById("meses").value);
 
     var baseCalculo = faturamentoTotal * 0.012;
-    var saldoMedio = (baseCalculo * meses) - 0.20;
-    
+    var saldoMedio = (baseCalculo * meses) - 0.20; // Subtrai 20%
+
     // Formatar o resultado sem arredondamento
     var saldoMedioFormatado = saldoMedio.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 
