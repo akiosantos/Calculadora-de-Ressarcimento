c53fd4c2bb82e6fba9b68704ff12d7f7554e9b8d
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-04-01 22:55:37 -0300

    Update app.py
    Alterando informação do Python

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -8,15 +8,15 @@ def index():
 
 @app.route('/calcular', methods=['POST'])
 def calcular():
-    faturamento_total = float(request.form['faturamento-total'])
-    meses = int(request.form['meses'])
+    dados = request.json
+    faturamento_total = dados['faturamento_total']
+    meses = dados['meses']
 
-    # Calcular o ressarcimento
     base_calculo = faturamento_total * 0.012
     honorarios = base_calculo * 0.20
     saldo_medio = (base_calculo * meses) - honorarios
 
-    return render_template('resultado.html', saldo_medio=saldo_medio)
+    return jsonify({'saldo_medio': saldo_medio})
 
 if __name__ == '__main__':
     app.run(debug=True)
